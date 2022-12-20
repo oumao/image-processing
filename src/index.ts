cd43e6e31e5dbf5df9b63b6dfbde96ec364372f1
oumao <evansotis2015@gmail.com>
2022-12-20 14:24:20 +0300

    Setting up the express server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-import express, {Request, Response} from 'express';
+// Third party imports
+import 'dotenv/config'
+import express from 'express'
+import { imageRoute } from './routes/image'
 
+// Local Imports
+
+const app = express()
+
+// Defining port
+const port = process.env.PORT ?? 3000 as number
+
+// Routes
+app.use('/api', imageRoute)
+
+// static folder
+app.use(express.static('public'))
+
+// Server Startup Function
+const start = (): void => {
+  app.listen(port, () => console.log(`Server Running on Port ${port}`))
+}
+
+start()
