8d8fa3193408ffae0e6d1872a8a94b4b0eb000c6
oumao <evansotis2015@gmail.com>
2022-12-24 20:08:49 +0300

    Cleaned imports and use prettier to format

--- src/middlewares/imageValidator.ts
+++ src/middlewares/imageValidator.ts
@@ -1,46 +1,45 @@
-import { Request, Response, NextFunction } from "express"
-import path from "path"
-import { promises as fsPromise } from "fs"
+import { Request, Response, NextFunction } from 'express'
+import path from 'path'
+import { promises as fsPromise } from 'fs'
 
+const validateImage = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  // Destructuring request query
+  const { name, height, width } = req.query
 
-const validateImage = async (req:Request, res: Response, next: NextFunction): Promise<void> => {  
+  const imgHeight = parseInt(height as string) // height of the image
+  const imgWidth = parseInt(width as string) // width of the image
 
-    // Destructuring request query
-    const { name, height, width } = req.query
+  // Extracting filenames available in public/images
+  const checkFiles = await fsPromise.readdir(
+    path.join(process.cwd(), 'public', 'images')
+  )
 
-    const imgHeight = parseInt(height as string) // height of the image
-    const imgWidth = parseInt(width as string) // width of the image
+  // Extracting filenames without extensions
+  const names: string[] = checkFiles.map((file) => path.basename(file, '.jpg'))
 
-    // Extracting filenames available in public/images
-    const checkFiles = await fsPromise.readdir(
-      path.join(process.cwd(), 'public', 'images')
-    )
-
-    // Extracting filenames without extensions
-    const names: string[] = checkFiles.map((file) =>
-      path.basename(file, '.jpg')
-    )
-
-    // boolean to check if name exists in extracted image files available at the public/image
-    const checkName: boolean = names.includes(name as string)
+  // boolean to check if name exists in extracted image files available at the public/image
+  const checkName: boolean = names.includes(name as string)
 
   // validate empty image name
-    if (name?.length === 0 || name === undefined) {
-        res.status(400).send('Image Name must be provided')
-      }
-  
-      // validate the height and width
-      if (isNaN(imgWidth) || isNaN(imgHeight)) {
-        res.status(400).send('The width and height must be a number')
-      }
-  
-      // Check for image name
-      if (!checkName) {
-        res.status(404).send('The Image name does not exist')
-      }
+  if (name?.length === 0 || name === undefined) {
+    res.status(400).send('Image Name must be provided')
+  }
 
-      next()
-      
+  // validate the height and width
+  if (isNaN(imgWidth) || isNaN(imgHeight)) {
+    res.status(400).send('The width and height must be a number')
+  }
+
+  // Check for image name
+  if (!checkName) {
+    res.status(404).send('The Image name does not exist')
+  }
+
+  next()
 }
 
 export default validateImage
