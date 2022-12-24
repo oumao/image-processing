626b452f37b7df0cc8cd8d797b61283ecaa10a0f
oumao <evansotis2015@gmail.com>
2022-12-24 19:54:54 +0300

    Added Image Validator middleware to the route

--- src/routes/image.ts
+++ src/routes/image.ts
@@ -1,9 +1,10 @@
 import express, { Router } from 'express'
 import convertImage from '../controllers/imagescontroller'
+import validateImage from '../middlewares/imageValidator'
 
 const imageRoute: Router = express.Router()
 
 // image route
-imageRoute.route('/images').get(convertImage)
+imageRoute.route('/images').get(validateImage, convertImage)
 
 export { imageRoute }
