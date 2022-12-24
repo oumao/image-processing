0280531c2fc9de034700faf1a2a0107516e12613
oumao <evansotis2015@gmail.com>
2022-12-24 19:54:24 +0300

    Refactor the validator to a middleware

--- src/controllers/imagescontroller.ts
+++ src/controllers/imagescontroller.ts
@@ -3,30 +3,18 @@ import { promises as fsPromise } from 'fs'
 import { Request, Response } from 'express'
 import imageResize from '../utils/imageResize'
 
+
 const convertImage = async (req: Request, res: Response): Promise<void> => {
   try {
 
     // Caching the response until new data is supplied
     res.set('Cache-control', 'private, must-revalidate')
     
-    // Destructuring request query
+    // // Destructuring request query
     const { name, height, width } = req.query
 
     const imgHeight = parseInt(height as string) // height of the image
     const imgWidth = parseInt(width as string) // width of the image
-
-    // Extracting filenames available in public/images
-    const checkFiles = await fsPromise.readdir(
-      path.join(process.cwd(), 'public', 'images')
-    )
-
-    // Extracting filenames without extensions
-    const names: string[] = checkFiles.map((file) =>
-      path.basename(file, '.jpg')
-    )
-
-    // boolean to check if name exists in extracted image files available at the public/image
-    const checkName: boolean = names.includes(name as string)
 
     // Getting the path to images
     const imagePath: string = path.join(
@@ -44,20 +32,6 @@ const convertImage = async (req: Request, res: Response): Promise<void> => {
       `${name as string}_thumb.png`
     )
 
-    // validate empty image name
-    if (name?.length === 0 || name === undefined) {
-      res.status(400).send('Image Name must be provided')
-    }
-
-    // validate the height and width
-    if (isNaN(imgWidth) || isNaN(imgHeight)) {
-      res.status(400).send('The width and height must be a number')
-    }
-
-    // Check for image name
-    if (!checkName) {
-      res.status(404).send('The Image name does not exist')
-    }
 
     // Function to convert the size of the image
     await imageResize(imagePath, imgWidth, imgHeight, finalPath)
