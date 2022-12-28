763be0015321bda6db5cd3f8847822c6a149083a
oumao <evansotis2015@gmail.com>
2022-12-28 23:29:50 +0300

    Added test for sharp library

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,5 +1,6 @@
 import supertest from 'supertest'
 import sizeOf from 'image-size'
+import imageResize from '../utils/imageResize'
 import fs from 'fs'
 import path from 'path'
 
@@ -21,11 +22,38 @@ describe('Image Resizing Route', () => {
 
     done()
   })
+
+  it('Should resize image using sharp', (done) => {
+    const imgName = 'fjord'
+    // Getting the path to images
+    const imagePath: string = path.join(
+      process.cwd(),
+      'public',
+      'images',
+      `${imgName as string}.jpg`
+    )
+
+    // Destination path of resized image
+    const finalPath: string = path.join(
+      process.cwd(),
+      'public',
+      'thumb',
+      `${imgName as string}_thumb.png`
+    )
+
+    imageResize(imagePath, 200, 200, finalPath)
+
+    expect(finalPath).toBeDefined()
+    expect(sizeOf(finalPath).width).toEqual(200)
+    expect(sizeOf(finalPath).height).toEqual(200)
+
+    done()
+  })
 })
 
 describe('Check Image Stats', () => {
   // Testing image existing
-  it('checks if the image file exists', () => {
+  it('Should checks if the image file exists', () => {
     const filePath = path.join(process.cwd(), 'public', 'images', 'fjord.jpg')
 
     // Use the fs.stat function to check the status of the file
@@ -40,7 +68,7 @@ describe('Check Image Stats', () => {
   })
 
   // Check if final resized image exists
-  it('checks if the final resized image file exists', () => {
+  it('should checks if the final resized image file exists', () => {
     const filePath = path.join(
       process.cwd(),
       'public',
@@ -59,7 +87,7 @@ describe('Check Image Stats', () => {
     })
   })
 
-  it('check if file is shrinked to 200 by 200', () => {
+  it('Should check if file is shrinked to 200 by 200', () => {
     // path to resized image
     const filePath = path.join(
       process.cwd(),
@@ -74,5 +102,22 @@ describe('Check Image Stats', () => {
     // If both width and height meets final dimensions
     expect(dimensions.width).toEqual(200)
     expect(dimensions.height).toBe(200)
+  })
+
+  it('Should check if file is greater than or equal to 50', () => {
+    // path to resized image
+    const filePath = path.join(
+      process.cwd(),
+      'public',
+      'thumb',
+      'palmtunnel_thumb.png'
+    )
+
+    // getting dimensions of the file
+    const dimensions = sizeOf(filePath)
+
+    // If both width and height meets final dimensions
+    expect(dimensions.width).toBeGreaterThanOrEqual(50)
+    expect(dimensions.height).toBeGreaterThanOrEqual(50)
   })
 })
